d52d9be7c471e7e9e105f8061285d248685eda1e
DiboraEyasuE <diboraeyasu792@gmail.com>
2025-11-01 23:56:29 +0300

    added

--- Learning_with___.py
+++ Learning_with___.py
@@ -4,7 +4,15 @@ data = {
     'Age' : [70, 45, 33, 31],
     'City': ['Addis Ababa', 'Bahir Dar', 'Gondar', 'Mekele']
 }
+books_data = {
+    'Title': ['The Great Gatsby', '1984', 'To Kill a Mockingbird', 'Pride and Prejudice'],
+    'Author': ['F. Scott Fitzgerald', 'George Orwell', 'Harper Lee', 'Jane Austen'],
+    'Year': [1925, 1949, 1960, 1813],
+    'Pages': [218, 328, 281, 432]
+}
 beteseb = pd.DataFrame(data)
 print(beteseb.shape)
-print(beteseb.head)
+print(beteseb.head(3))
 print(beteseb.columns.to_list())
+Metsaf = pd.DataFrame(books_data)
+print(Metsaf.shape) 
